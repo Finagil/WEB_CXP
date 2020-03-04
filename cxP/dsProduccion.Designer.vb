db3ef4f34d37ee43a1f9779dcbe0132ac5e50304
ecacerest <ecacerest@finagil.com.mx>
2020-03-04 09:46:19 -0600

    Correcion de agruoamiento por cancelacion en fecha diferente de la solicitud

--- cxP/dsProduccion.Designer.vb
+++ cxP/dsProduccion.Designer.vb
@@ -39309,11 +39309,11 @@ Namespace dsProduccionTableAdapters
             Me._commandCollection(0).Parameters.Add(New Global.System.Data.SqlClient.SqlParameter("@empresa", Global.System.Data.SqlDbType.[Decimal], 9, Global.System.Data.ParameterDirection.Input, 18, 0, "idEmpresas", Global.System.Data.DataRowVersion.Current, false, Nothing, "", "", ""))
             Me._commandCollection(1) = New Global.System.Data.SqlClient.SqlCommand()
             Me._commandCollection(1).Connection = Me.Connection
-            Me._commandCollection(1).CommandText = "SELECT        Autoriza1, Autoriza2, estatus, fechaSolicitud, folioSolicitud, razo"& _ 
-                "nSocial, MIN(st) AS st"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)&"FROM            Vw_CXP_MisSolicitudes"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)&"WHERE        (usu"& _ 
-                "ario = @usuario) AND (idEmpresas = @empresa) AND (folioSolicitud = @folioSolicit"& _ 
-                "ud)"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)&"GROUP BY Autoriza1, Autoriza2, estatus, fechaSolicitud, folioSolicitud, raz"& _ 
-                "onSocial"
+            Me._commandCollection(1).CommandText = "SELECT        Autoriza1, Autoriza2, estatus, MAX(fechaSolicitud) AS fechaSolicitu"& _ 
+                "d, folioSolicitud, razonSocial, MIN(st) AS st"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)&"FROM            Vw_CXP_MisSolicit"& _ 
+                "udes"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)&"WHERE        (usuario = @usuario) AND (idEmpresas = @empresa) AND (folioSo"& _ 
+                "licitud = @folioSolicitud)"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)&"GROUP BY Autoriza1, Autoriza2, estatus, folioSolicit"& _ 
+                "ud, razonSocial"
             Me._commandCollection(1).CommandType = Global.System.Data.CommandType.Text
             Me._commandCollection(1).Parameters.Add(New Global.System.Data.SqlClient.SqlParameter("@usuario", Global.System.Data.SqlDbType.VarChar, 50, Global.System.Data.ParameterDirection.Input, 0, 0, "usuario", Global.System.Data.DataRowVersion.Current, false, Nothing, "", "", ""))
             Me._commandCollection(1).Parameters.Add(New Global.System.Data.SqlClient.SqlParameter("@empresa", Global.System.Data.SqlDbType.[Decimal], 9, Global.System.Data.ParameterDirection.Input, 18, 0, "idEmpresas", Global.System.Data.DataRowVersion.Current, false, Nothing, "", "", ""))
